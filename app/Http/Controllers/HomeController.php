c3b2218d1e21678d68f9550365992cc97ada9438
Marcelo Espinoza <mespinozauarez@gmail.com>
2018-11-06 22:36:37 -0300

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Certificado;
+use Illuminate\Support\Facades\Storage;
+
+class HomeController extends Controller
+{
+    public function generateDocx()
+    {
+    	 $template = new \PhpOffice\PhpWord\TemplateProcessor('template.docx');
+
+         $template->setValue('numberOfCertificate','8511261-15');
+
+         $template->setValue('test','8511261-15');
+    	 $template->setValue('address', 'Alcántara N° 44, Piso 7, Las Condes, Santiago Alcántara N° 44, Piso 7, Las Condes, Santiago Alcántara N° 44, Piso 7, Las Condes, Santiago');
+
+    	 $name = 'nombre.docx';
+
+		try {
+            $template->saveAs(storage_path($name));
+        } catch (Exception $e) {
+        }
+
+        return response()->download(storage_path($name));        
+    
+    }
+
+    public function welcome()
+    {
+
+        return view('welcome');
+    }
+
+    public function data()
+    {
+        $data = '[';
+        foreach (Certificado::get() as $key => $value) {
+
+            $data.= "{value:'".$value->modelo."'},";
+        }
+        $data = substr($data, 0, -1);
+        $data .= ']';
+        return json_encode($data);
+    }
+
+
+}
